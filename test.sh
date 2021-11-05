9d6a5016532a3b08ebf119151e54515f726a7fc4
Michael Lingelbach <m.j.lbach@gmail.com>
2021-11-04 21:04:07 -0700

    feat: add test suite with plenary

---
+++ test.sh
@@ -0,0 +1,1 @@
+nvim --headless -c "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minimal_init.lua'}"
---
+++ tests/incremental_sync_spec.lua
@@ -0,0 +1,25 @@
+local sync = require('sync')
+
+describe("One edit operations", function()
+  it("Append a character on the current line at the 0th position", function()
+    local prev_lines = {
+      '',
+    }
+    local curr_lines = {
+      'a',
+    }
+    local firstline = 0
+    local lastline = 1
+    local new_lastline = 1
+    local offset_encoding="utf-8"
+    local line_ending="\n"
+    local text_edit = sync.compute_diff(prev_lines, curr_lines, firstline, lastline, new_lastline, offset_encoding, line_ending)
+
+    -- Hacky adapter to old sync assumptions
+    local startline =  math.min(firstline + 1, math.min(#prev_lines, #curr_lines))
+    local endline =  math.min(-(#curr_lines - new_lastline), -1)
+    local text_edit_original = vim.lsp.util.compute_diff(prev_lines, curr_lines, startline, endline, offset_encoding)
+    assert.equals(text_edit, text_edit_original)
+  end)
+
+end)
